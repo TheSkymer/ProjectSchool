c385d1e57a41d412c904ef853c28f2971a67e70b
TheSkymer <andrea.toccaceli@live.com>
2015-05-14 19:58:20 +0200

    improving Conf.ProjectSchool.py for type(file)

--- Source/Conf.ProjectSchool.py
+++ Source/Conf.ProjectSchool.py
@@ -1,17 +1,17 @@
 #Conf for ProjectSchool  
 #!/home/andrea
 percorso_file = {1 : 'materie.txt', 2 : 'voti.txt'}
-conf = open(percorso_file[1], 'w')  
+conf = open(percorso_file[1], 'w')
 voti = open(percorso_file[2],'w')  
-memo = open('memo.txt', 'w')  
-memo.write(percorso_file[1] + ',')  
-memo.write(percorso_file[2])  
-memo.close()  
+with open('memo.txt', 'w') as memo:  
+    memo.write(percorso_file[1] + ',')  
+    memo.write(percorso_file[2])  
 print('Benvenuto Utente')
 print('')
 print('Stai configurando ProjectSchool per i tuoi usi')
-mat_1 = int(input('Quante materie hai: ')) + 1  
-for mat in range(1, mat_1):  
+numero_materie = input('Quante materie hai: ')   
+numero_materie = int(numero_materie) + 1
+for materie in range(1, numero_materie):  
     i = input('Digita il nome della materia: ') + ','
     var = '0' + '\n' 
     conf.write(i)  
